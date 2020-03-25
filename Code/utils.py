86165044f25afd9675840e90ccfd7c53200bebbe
Arpit Aggarwal <arpitdec5@gmail.com>
2020-03-25 18:55:34 -0400

    Added utils.py file and added output folder

---
+++ Code/utils.py
@@ -0,0 +1,17 @@
+# header files
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+
+# function to plot the average color histogram of image
+def plot_hist(image):
+    # loop over the image channels
+    chans = cv2.split(image)
+    colors = ("b", "g", "r")
+    features = []
+    for (chan, color) in zip(chans, colors):
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        hist = hist / hist.sum()
+        features.extend(hist)
+        plt.plot(hist, color = color)
+        plt.xlim([0, 256])
