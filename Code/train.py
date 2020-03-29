5af971ad945b3659b6b89c1a26722d821a84d7c8
Arpit Aggarwal <arpitdec5@gmail.com>
2020-03-29 19:26:14 -0400

    Updated train.py file

--- Code/train.py
+++ Code/train.py
@@ -4,9 +4,13 @@ from matplotlib import pyplot as plt
 import cv2
 from roipoly import roipoly
 import pylab as pl
+import sys
 
 # set data path
-path_video = "/mnt/c/Users/shant/Underwater_Color_Segmentation_GMM_EM_Algorithm/data/detectbuoy.avi"
+args = sys.argv
+if(len(args) > 2):
+    path_video = args[1]
+    output_path = args[2]
 
 # define constants
 cap = cv2.VideoCapture(path_video)
@@ -15,7 +19,6 @@ cap = cv2.VideoCapture(path_video)
 count = 0
 while(cap.isOpened()):
     ret, frame = cap.read()    
-
     if(ret):
         image1 = frame.copy()
 
@@ -34,57 +37,11 @@ while(cap.isOpened()):
         (x,y), radius = cv2.minEnclosingCircle(points)
         center = (int(x),int(y))
         radius = int(radius)
-
         rectX = (center[0] - radius)
         rectY = (center[1] - radius)
 
         cropped = image1[rectY:rectY + 2*radius, rectX:rectX + 2*radius].copy()
-
-        cv2.imwrite("/mnt/c/Users/shant/Underwater_Color_Segmentation_GMM_EM_Algorithm/data/buoy2/buoy" + str(count) + ".png", cropped)
-        
-        #cv2.imshow("Image", cropped)
-        #cv2.waitKey(0)
-
-        # draw polygon on second buoy
-        '''
-        pl.imshow(image2.copy(), interpolation='nearest', cmap="Greys")
-        pl.colorbar()
-        pl.title("left click: line segment         right click: close region")
-        ROI2 = roipoly(roicolor='r')
-        pl.imshow(image2.copy(), interpolation='nearest', cmap="Greys")
-        pl.colorbar()
-        ROI2.displayROI()
-        pl.title('draw second ROI')
-
-        # save second buoy image
-        points = np.array(ROI2.get_points(image2.copy()))
-        rect = cv2.boundingRect(points)
-        x, y, w, h = rect
-        cropped = image2[y:y+h, x:x+w].copy()
-        cv2.imwrite("data/buoy2/buoy" + str(count) + ".png", cropped)
-        cv2.imshow("Image", cropped)
-        cv2.waitKey(0)
-
-        # draw polygon on third buoy
-        pl.imshow(image3.copy(), interpolation='nearest', cmap="Greys")
-        pl.colorbar()
-        pl.title("left click: line segment         right click: close region")
-        ROI3 = roipoly(roicolor='r')
-        pl.imshow(image3.copy(), interpolation='nearest', cmap="Greys")
-        pl.colorbar()
-        ROI3.displayROI()
-        pl.title('draw second ROI')
-
-        # save third buoy image
-        points = np.array(ROI3.get_points(image3.copy()))
-        rect = cv2.boundingRect(points)
-        x, y, w, h = rect
-        cropped = image3[y:y+h, x:x+w].copy()
-        cv2.imwrite("data/buoy3/buoy" + str(count) + ".png", cropped)
-        cv2.imshow("Image", cropped)
-        cv2.waitKey(0)
-        break
-        '''
+        cv2.imwrite(str(output_path) + "/buoy" + str(count) + ".png", cropped)
         count = count + 1
     else:
         break
