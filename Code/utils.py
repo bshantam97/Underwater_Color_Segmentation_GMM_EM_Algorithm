1ce13758619bbcd2a04aacfbb0c44b0bfa2d66df
Arpit Aggarwal <arpitdec5@gmail.com>
2020-03-31 18:53:12 -0400

    Updated notebook and utils.py

--- Code/utils.py
+++ Code/utils.py
@@ -28,16 +28,18 @@ def initialise_step(n, d, k):
         
     # return the arrays
     return (weights_gaussian, mean_gaussian, covariance_matrix_gaussian, probability_values)
-    
+
+
 # gaussian estimation for expectation step
 def gaussian_estimation(data_point, mean, covariance, dimension):
     determinant_covariance = np.linalg.det(covariance)
     determinant_covariance_root = np.sqrt(determinant_covariance)
     covariance_inverse = np.linalg.inv(covariance)
-    gaussian_pi_coeff = 1 / np.power((2 * np.pi), (dimension / 2))
+    gaussian_pi_coeff = 1.0 / np.power((2 * np.pi), (dimension / 2))
     data_mean_diff = (data_point - mean)
     data_mean_diff_transpose = data_mean_diff.T     
-    return (gaussian_pi_coeff) * (determinant_covariance_root) * np.exp(-0.5 * np.matmul(np.matmul(data_mean_diff_transpose, covariance_inverse), data_mean_diff))
+    return (gaussian_pi_coeff) * (determinant_covariance_root) * np.exp(-0.5 * np.matmul(np.matmul(data_mean_diff, covariance_inverse), data_mean_diff_transpose))
+
 
 # e-step of the algorithm
 # reference: https://towardsdatascience.com/an-intuitive-guide-to-expected-maximation-em-algorithm-e1eb93648ce9
@@ -46,10 +48,61 @@ def expectation_step(n, d, k, data, weights_gaussian, mean_gaussian, covariance_
     for j in range(0, n):
         probability_x = 0.0
         for i in range(0, k):
-            probability_x = probability_x + gaussian_estimation(data.iloc[j], mean_gaussian[i], covariance_matrix_gaussian[i] * weights_gaussian[i], d)
+            probability_x = probability_x + gaussian_estimation(data[j], mean_gaussian[i], covariance_matrix_gaussian[i] * weights_gaussian[i], d)
         probability_x_temp=[]    
         for i in range(k):
-            val = gaussian_estimation(data.iloc[j], mean_gaussian[i], covariance_matrix_gaussian[i] * weights_gaussian[i], d) / probability_x
+            val = gaussian_estimation(data[j], mean_gaussian[i], covariance_matrix_gaussian[i] * weights_gaussian[i], d) / probability_x
             probability_x_temp.append(val)
         probabilities.append(probability_x_temp)
-    return pd.DataFrame(probabilities)
+    return np.array(probabilities)
+
+
+# update weights, maximization step
+def update_weights(probabilities, k):
+    probabilities = np.array(probabilities)
+    updated_weights = []
+    for i in range(0, k):
+        updated_weights.append(np.sum(probabilities[:, i]))
+    updated_weights = np.array(updated_weights)
+    return updated_weights / np.sum(updated_weights)
+
+
+# update mean, maximization step
+def update_mean(data, probabilities, k):
+    probabilities = np.array(probabilities)
+    data = np.array(data)
+    updated_weights = []
+    updated_mean = np.matmul(probabilities.T, data)
+    for i in range(0, k):
+        updated_weights.append(np.sum(probabilities[:, i]))
+        updated_mean[i] = updated_mean[i] / updated_weights[i]
+    return updated_mean
+
+# update covariance, maximization step
+def update_covariance(data, probabilities, mean_gaussian, k):
+    return data
+
+# m-step of the algorithm
+# reference: https://towardsdatascience.com/an-intuitive-guide-to-expected-maximation-em-algorithm-e1eb93648ce9
+def maximization_step(n, d, k, data, weights_gaussian, mean_gaussian, covariance_matrix_gaussian, probability_values):
+    u_weights = update_weights(probability_values, k)
+    u_mean_gaussian = update_mean(data, probabilities, k)
+    u_covariance_matrix_gaussian = update_covariance(data, probabilities, mean_gaussian, k)
+    return (u_weights, u_mean_gaussian, u_covariance_matrix_gaussian)
+
+# run e-m algorithm
+def run_expectation_maximization_algorithm(n, d, k, iterations, data):
+    # initialise step
+    (weights_gaussian, mean_gaussian, covariance_matrix_gaussian, probability_values) = initialise_step(n, d, k)
+    
+    # run for fixed iterations
+    for i in range(0, iterations):
+    
+        # m-step
+        (weights_gaussian, mean_gaussian, covariance_matrix_gaussian) = maximization_step(n, d, k, data, weights_gaussian, mean_gaussian, covariance_matrix_gaussian, probability_values)
+    
+        # e-step
+        probability_values = expectation_step(n, d, k, data, weights_gaussian, mean_gaussian, covariance_matrix_gaussian, probability_values)
+    
+    # return answer
+    return (weights_gaussian, mean_gaussian, covariance_matrix_gaussian)
